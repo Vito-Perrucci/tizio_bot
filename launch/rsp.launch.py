2d667ab37fe5cab98d1e15f25d868c33e8d360ae
Vito-Perrucci <vito-perrucci@live.it>
2025-09-07 19:04:07 +0200

    robot definition and creation of launch files

---
+++ launch/rsp.launch.py
@@ -0,0 +1,39 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+import xacro
+
+def generate_launch_description():
+
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Process the URDF file for the description of the robot
+    pkg_path = os.path.join(get_package_share_directory('tizio_bot'))
+    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    
+    # creation of a state publisher node
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    # and launch
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+
+        node_robot_state_publisher
+    ])
